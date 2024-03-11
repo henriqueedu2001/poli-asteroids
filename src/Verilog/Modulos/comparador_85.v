551b9093c3ea93af44de3d5ecaae9ca5ed3b6971
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2024-03-10 21:20:54 -0300

    comit

---
+++ src/Verilog/Modulos/comparador_85.v
@@ -0,0 +1,14 @@
+module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
+
+    input[3:0] A, B;
+    input      ALBi, AGBi, AEBi;
+    output     ALBo, AGBo, AEBo;
+    wire[4:0]  CSL, CSG;
+
+    assign CSL  = ~A + B + ALBi;
+    assign ALBo = ~CSL[4];
+    assign CSG  = A + ~B + AGBi;
+    assign AGBo = ~CSG[4];
+    assign AEBo = ((A == B) && AEBi);
+
+endmodule /* comparador_85 */
