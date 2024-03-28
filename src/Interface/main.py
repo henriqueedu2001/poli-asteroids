066885d97e69b5b33e62f94fa285af815b6a0693
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2024-03-28 11:12:09 -0300

    integration uart + pygame

--- src/Interface/main.py
+++ src/Interface/main.py
@@ -1,4 +1,5 @@
 import pygame
+import utilitary.uart as uart
 
 # tamanho da tela
 SCREEN_WIDHT = 800
@@ -25,15 +26,34 @@ def run_game(screen, clock):
       clock (pygame.time.Clock): clock do jogo
   """
   run = True
+
+  i = 0
+  send_data_buffer = ['a', 'b', 'c', 'd']
   
   while run:
+    send_data = send_data_buffer[i%4]
+    received_data = transmit_data(send_data)
+    print(received_data)
+    i = i + 1
+
     for event in pygame.event.get():
-      
       # lógica de fim do jogo
       if event.type == pygame.QUIT:
         run = False
   
   # sair do jogo
   pygame.quit()
+
+
+def transmit_data(send_data: str):
+  port = uart.open_port(uart.DEFAULT_PORT_NAME)
+
+  # recebimento de dados
+  received_data = uart.receive_data(port)
+
+  # transmissão de dados
+  uart.send_data(port, send_data)
+
+  return received_data
 
 start_game()
