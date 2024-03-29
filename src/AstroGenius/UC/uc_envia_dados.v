75ebf2017e682e11cb175d168806b2ceedda5452
jofe2003 <jo.fe2003@usp.br>
2024-03-29 12:37:59 -0300

    estados da uc_envia_dados.v em ordem

--- src/AstroGenius/UC/uc_envia_dados.v
+++ src/AstroGenius/UC/uc_envia_dados.v
@@ -10,47 +10,45 @@ module uc_renderiza (
         parameter inicial                         = 6'b000000; // 0
         parameter espera                          = 6'b000001; // 1
         parameter zera_contadores                 = 6'b000010; // 2
-        parameter inicia_envio_de_cabecalho       = 6'b000011; // 3
-        parameter espera_envio_de_cabecalho       = 6'b000100; //4
-        parameter verifica_rco_contador_cabecalho = 6'b000101; // 5
-        parameter incrementa_contador_cabecalho   = 6'b000110; // 6
-        parameter inicia_envio_de_pontuacao       = 6'b000111; // 7
-        parameter espera_envio_de_pontuacao       = 6'b001000; // 8
-        parameter envio_opcode_nave               = 6'b001001; // 9
-        parameter inicia_envio_opcode_nave        = 6'b001010; // 10 - esse estado envia também opcode_nave, vidas e dificuldade
-        parameter espera_envio_opcode_nave        = 6'b001011; // 11
-        parameter envia_posicao_nave              = 6'b001100; // 12
-        parameter inicia_envia_posicao_aste       = 6'b001101; // 13
-        parameter espera_envia_posicao_aste       = 6'b001110; // 14
-        parameter verifica_rco_aste               = 6'b001111; // 15
-        parameter incrementa_contador_asteroides  = 6'b010000; // 16
-        parameter espera_mem_aste                 = 6'b010001; // 17
-        parameter envia_opcode_aste               = 6'b010010; // 18
-        parameter inicia_envia_opcode_aste        = 6'b010011; // 19
-        parameter espera_envia_opcode_aste        = 6'b010100; // 20
-        parameter verifica_rco_contador_byte_opcode_aste = 6'b010101; // 21
-        parameter incrementa_contador_opcode_byte = 6'b010110; // 22
-        parameter envia_posicao_tiros             = 6'b010111; // 23
-        parameter inicia_envia_posicao_tiros      = 6'b011000; // 24
-        parameter espera_envia_posicao_tiros      = 6'b011001; // 25
-        parameter verifica_rco_tiro               = 6'b011010; // 26
-        parameter incrementa_contador_tiros       = 6'b011011; // 27
-        parameter espera_mem_tiros                = 6'b011100; // 28
-        parameter envia_opcode_tiro               = 6'b011101; // 29
-        parameter inicia_envia_opcode_tiro        = 6'b011110; // 30
-        parameter espera_envia_opcode_tiro        = 6'b011111; // 31
-        parameter verifica_rco_contador_byte_opcde_tiro = 6'b100000; // 32
-        parameter envia_jogada_especial        = 6'b100001; // 33
-        parameter inicia_envia_jogada_especial = 6'b100010; // 34
-        parameter espera_envia_jogada_especial = 6'b100011; // 35
-        parameter sinaliza                     = 6'b100100; // 36
-        parameter erro                         = 6'b111111; // 63
-
-
+        parameter inicia_envio_de_pontuacao       = 6'b000011; // 3
+        parameter espera_envio_de_pontuacao       = 6'b000100; // 4
+        parameter envio_opcode_nave               = 6'b000101; // 5
+        parameter inicia_envio_opcode_nave        = 6'b000110; // 6
+        parameter espera_envio_opcode_nave        = 6'b000111; // 7
+        parameter envia_posicao_aste              = 6'b001000; // 8
+        parameter inicia_envia_posicao_aste       = 6'b001001; // 9
+        parameter espera_envia_posicao_aste       = 6'b001010; // 10
+        parameter verifica_rco_aste               = 6'b001011; // 11
+        parameter incrementa_contador_asteroides  = 6'b001100; // 12
+        parameter espera_mem_aste                 = 6'b001101; // 13
+        parameter envia_opcode_aste               = 6'b001110; // 14
+        parameter inicia_envia_opcode_aste        = 6'b001111; // 15
+        parameter espera_envia_opcode_aste        = 6'b010000; // 16
+        parameter verifica_rco_contador_byte_opcode_aste = 6'b010001; // 17
+        parameter incrementa_contador_byte_opcode_aste = 6'b010010; // 18
+        parameter envia_posicao_tiros             = 6'b010011; // 19
+        parameter inicia_envia_posicao_tiros      = 6'b010100; // 20
+        parameter espera_envia_posicao_tiros      = 6'b010101; // 21
+        parameter verifica_rco_tiro               = 6'b010110; // 22
+        parameter incrementa_contador_tiros       = 6'b010111; // 23
+        parameter espera_mem_tiros                = 6'b011000; // 24
+        parameter envia_opcode_tiro               = 6'b011001; // 25
+        parameter inicia_envia_opcode_tiro        = 6'b011010; // 26
+        parameter espera_envia_opcode_tiro        = 6'b011011; // 27 
+        parameter verifica_rco_contador_byte_opcde_tiro = 6'b011100; // 28
+        parameter incrementa_contador_byte_opcode_tiro = 6'b011101; // 29
+        parameter envia_jogada_especial            = 6'b011110; // 30
+        parameter inicia_envia_jogada_especial     = 6'b011111; // 31 
+        parameter espera_envia_jogada_especial     = 6'b100000; // 32 
+        parameter inicia_envio_de_rodape           = 6'b100001; // 33
+        parameter espera_envio_de_rodape           = 6'b100010; // 34
+        parameter verifica_rco_contador_rodape     = 6'b100011; // 35
+        parameter incrementa_contador_rodape       = 6'b100100; // 36
+        parameter sinaliza                         = 6'b100101; // 37
+        parameter erro                             = 6'b111111; // 64
 
         // Variáveis de estado
         reg [3:0] estado_atual, proximo_estado;
-
 
         // Memória de estado
         always @(posedge clock or posedge reset) begin
